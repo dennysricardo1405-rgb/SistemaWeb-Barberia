ecb3ad93e26b007112206a2fbbfe642a5f6f242a
Dennys Lozano <dennyricardo1405@gmail.com>
2026-06-05 12:40:03 -0500

    Modificar el DataInitializer

--- src/main/java/com/example/BarberiaLaClasica/repository/PermisoRepository.java
+++ src/main/java/com/example/BarberiaLaClasica/repository/PermisoRepository.java
@@ -1,5 +1,7 @@
 package com.example.BarberiaLaClasica.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,5 @@ import com.example.BarberiaLaClasica.model.Permiso;
 
 @Repository
 public interface PermisoRepository extends JpaRepository<Permiso, Long> {
-
-    
-} 
+    Optional<Permiso> findByNombrePermiso(String nombrePermiso);
+}
